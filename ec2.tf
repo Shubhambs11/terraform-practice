2f4ca484d971e00332a9b40881b8cf1270edc5ed
Shubham Suryawanshi <Shubhambs1106@gmail.com>
2026-04-03 21:59:27 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -78,8 +78,13 @@ resource "aws_instance" "my_instance" {
   tags = {
     Name = "terra-automate-server"
   }
-
 }
+
+resource "aws_ec2_instance_state" "my_instance_state" {
+     instance_id = aws_instance.my_instance.id
+     state = "stopped"
+}
+
 
 
 
